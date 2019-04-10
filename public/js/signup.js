f6a7280d3879f7b6c9e6ed8bda38be59a5eae77d
Yadi <ashleyfreedenberg@gmail.com>
2019-04-10 10:52:26 -0700

    first pull 4/10

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,13 +1,25 @@
-$("#user-sign-up").on("submit", function(e) {
+$("#user-sign-up").on("click", function(e) {
   e.preventDefault();
   $.ajax({
     method: "POST",
     url: "/api/signup",
     data: {
+      name: $("#name")
+        .val()
+        .trim(),
       email: $("#email")
         .val()
         .trim(),
+      username: $("#username")
+        .val()
+        .trim(),
       password: $("#password")
+        .val()
+        .trim(),
+      instagram: $("#instagram")
+        .val()
+        .trim(),
+      facebook: $("#facebook")
         .val()
         .trim()
     }
