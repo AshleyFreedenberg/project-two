ab686440ed5cd434981e5f67d375a62621b81d21
Yadi <ashleyfreedenberg@gmail.com>
2019-04-10 14:23:27 -0700

    making everything on one page

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,35 +1,52 @@
-$("#user-sign-up").on("click", function(e) {
-  e.preventDefault();
-  $.ajax({
-    method: "POST",
-    url: "/api/signup",
-    data: {
-      name: $("#name")
-        .val()
-        .trim(),
-      email: $("#email")
-        .val()
-        .trim(),
-      username: $("#username")
-        .val()
-        .trim(),
-      password: $("#password")
-        .val()
-        .trim(),
-      instagram: $("#instagram")
-        .val()
-        .trim(),
-      facebook: $("#facebook")
-        .val()
-        .trim()
-    }
-  })
-    .then(function(data) {
-      console.log(data);
-      window.location.replace(data);
+$(document).ready(function() {
+  function start() {
+    $("#signUpForm").hide();
+    $("#login").hide();
+  }
+  start();
+
+  $("#signup").on("click", function(e) {
+    e.preventDefault();
+    $("#signUpForm").show();
+    $("#login").hide();
+    $("#signUpForm")[0].scrollIntoView();
+  });
+
+  $("#user-sign-up").on("click", function(e) {
+    e.preventDefault();
+    $.ajax({
+      method: "POST",
+      url: "/api/signup",
+      data: {
+        name: $("#name")
+          .val()
+          .trim(),
+        email: $("#email")
+          .val()
+          .trim(),
+        username: $("#username")
+          .val()
+          .trim(),
+        password: $("#password")
+          .val()
+          .trim(),
+        instagram: $("#instagram")
+          .val()
+          .trim(),
+        facebook: $("#facebook")
+          .val()
+          .trim()
+      }
     })
-    .catch(function(err) {
-      console.log(err);
-      alert(err.responseText);
-    });
-});
+      .then(function (data) {
+        console.log(data);
+        window.location.replace(data);
+        start();
+      })
+      .catch(function (err) {
+        console.log(err);
+        alert(err.responseText);
+      });
+  });
+
+})
